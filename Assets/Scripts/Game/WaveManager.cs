fd6df80add9978429c10ff1a687e66972eef60ba
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-07-23 18:35:29 -0400

    finished wave manager

--- Assets/Scripts/Game/WaveManager.cs
+++ Assets/Scripts/Game/WaveManager.cs
@@ -12,10 +12,24 @@ public struct WaveGroup {
 
 public class WaveManager : MonoBehaviour
 {
-    public static Queue<WaveGroup> waveQueue = new Queue<WaveGroup>();
+    public static WaveManager Global;
+
+    public Queue<WaveGroup> waveQueue = new Queue<WaveGroup>();
+    public bool isWaveOngoing = false;
+    public Transform tempSpawnPoint;
+
+    void Start() {
+        WaveManager.Global = this;
+
+        WaveManager.Global.queueUnit("TestEnemy", 5, 0, 0.2f);
+        WaveManager.Global.queueUnit("TestEnemy2", 5, 1, 1f);
+        WaveManager.Global.queueUnit("TestEnemy", 10, 1, 0.2f);
+        WaveManager.Global.startWave();
+
+    }
 
     public void startWave() {
-        if (!WaveManager.isWaveOnGoing()) {
+        if (WaveManager.Global.isWaveOngoing) {
             Debug.LogError("There is already a wave ongoing.");
             return;
         }
@@ -24,13 +38,17 @@ public class WaveManager : MonoBehaviour
 
     private IEnumerator spawnUnitWave() {
 
-        while (WaveManager.isWaveOnGoing()) {
-            WaveGroup waveInfo = WaveManager.waveQueue.Dequeue();
+        WaveManager.Global.isWaveOngoing = true;
+
+        while (WaveManager.Global.waveQueue.Count > 0) {
+            WaveGroup waveInfo = WaveManager.Global.waveQueue.Dequeue();
 
             StartCoroutine(spawnUnits(waveInfo));
 
             yield return new WaitForSeconds(waveInfo.pad);
         }
+
+        WaveManager.Global.isWaveOngoing = false;
     }
 
     private IEnumerator spawnUnits(WaveGroup waveInfo) {
@@ -38,22 +56,25 @@ public class WaveManager : MonoBehaviour
         for (int i = 0; i < waveInfo.count; i++) {
 
             // create enemy gameobjects and set them up
-            // GameObject newEnemy = Instantiate();
+            GameObject newEnemy = (GameObject)Instantiate(Resources.Load(waveInfo.prefabName));
+            newEnemy.transform.position = WaveManager.Global.tempSpawnPoint.transform.position;
+            newEnemy.GetComponent<PathTraveller>().destinationNode = WaveManager.Global.tempSpawnPoint.GetComponent<PathNode>();
 
             yield return new WaitForSeconds(waveInfo.stagger);
         }
     }
 
-    public static bool isWaveOnGoing() {
-        return WaveManager.waveQueue.Count > 0;
+    public void queueUnit(string prefabName, int count, float pad, float stagger) {
+        WaveManager.Global.waveQueue.Enqueue(new WaveGroup {prefabName=prefabName, count=count, pad=pad, stagger=stagger});
     }
 
-    public static void queueUnit(string prefabName, int count, float pad, float stagger) {
-        WaveManager.waveQueue.Enqueue(new WaveGroup {prefabName=prefabName, count=count, pad=pad, stagger=stagger});
-    }
-
-    public static void spawnUnit<T>(int count) {
-
+    // spawn units without waiting
+    public void spawnUnit(string prefabName, int count, float stagger) {
+        if (!WaveManager.Global.isWaveOngoing) {
+            Debug.LogError("There is no ongoing wave.");
+            return;
+        }
+        StartCoroutine(spawnUnits(new WaveGroup {prefabName=prefabName, count=count, pad=0, stagger=stagger}));
    }
 
 }
