1297a3255f599cda0cce4132ff7ce2d0dd1fca61
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-07-24 00:04:15 -0400

    merge

--- Assets/Scripts/Tower/TowerManager.cs
+++ Assets/Scripts/Tower/TowerManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerSpawner : MonoBehaviour
+public class TowerManager: MonoBehaviour
 {
     GameObject tower;
 
